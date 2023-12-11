3b6291f3a2eec75c918e5bd5a4bcf9226071260d
Tiego Ouedraogo <tiegoouedraogo@gmail.com>
2023-12-10 19:35:23 -0500

    change words to presidents of the USA

--- script.js
+++ script.js
@@ -1,185 +1,129 @@
 let wordList = [
-    {
-      word: "wonderful",
-      hint: "An adjective describing something inspiring delight",
-    },
-    { word: "apple", hint: "A common fruit known for its round shape" },
-    { word: "classroom", hint: "A room where lessons are taught" },
-    { word: "rude boy", hint: "A term originating from Jamaican street culture" },
-    { word: "happy", hint: "A feeling of joy or pleasure" },
-    {
-      word: "forgiveness",
-      hint: "The action of pardoning someone for a mistake or wrongdoing",
-    },
-    { word: "college", hint: "An educational institution or establishment" },
-    {
-      word: "fired",
-      hint: "Losing one's job; often used in the phrase 'You're ___'",
-    },
-    {
-      word: "world",
-      hint: "The earth, together with all of its countries and peoples",
-    },
-    {
-      word: "space",
-      hint: "A vast, seemingly infinite area beyond Earth's atmosphere",
-    },
-    { word: "clown", hint: "A comic entertainer, especially one in a circus" },
-    { word: "love", hint: "An intense feeling of deep affection" },
-    { word: "eat", hint: "To put food into the mouth and chew and swallow it" },
-    { word: "mistake", hint: "An action or judgment that is misguided or wrong" },
-    {
-      word: "ocean",
-      hint: "A vast body of salt water that covers almost three-quarters of the earth's surface",
-    },
-    {
-      word: "mountain",
-      hint: "A large natural elevation of the earth's surface rising abruptly from the surrounding level",
-    },
-    {
-      word: "desert",
-      hint: "A barren area of landscape where little precipitation occurs",
-    },
-    {
-      word: "forest",
-      hint: "A large area covered chiefly with trees and undergrowth",
-    },
-    {
-      word: "river",
-      hint: "A large natural stream of water flowing in a channel to the sea, a lake, or another river",
-    },
-    {
-      word: "volcano",
-      hint: "A mountain or hill, typically conical, having a crater or vent through which lava is erupted",
-    },
-    { word: "island", hint: "A piece of land surrounded by water" },
-    {
-      word: "glacier",
-      hint: "A slowly moving mass or river of ice formed by the accumulation and compaction of snow",
-    },
-    {
-      word: "jungle",
-      hint: "Land covered with dense vegetation dominated by trees",
-    },
-    { word: "city", hint: "A large town" },
-    {
-      word: "village",
-      hint: "A group of houses and associated buildings, smaller than a town",
-    },
-    {
-      word: "eclipse",
-      hint: "An event in which the shadow of one celestial body falls on another",
-    },
-    {
-      word: "galaxy",
-      hint: "A system of millions or billions of stars, together with gas and dust, held together by gravitational attraction",
-    },
-    {
-      word: "universe",
-      hint: "All existing matter and space considered as a whole",
-    },
-    {
-      word: "nature",
-      hint: "The phenomena of the physical world collectively, including plants, animals, and the landscape",
-    },
-    {
-      word: "animal",
-      hint: "A living organism that feeds on organic matter, typically having specialized sense organs and nervous system",
-    },
-    {
-      word: "mammal",
-      hint: "A warm-blooded vertebrate animal distinguished by the possession of hair or fur",
-    },
-    {
-      word: "reptile",
-      hint: "A cold-blooded vertebrate of a class that includes snakes, lizards, crocodiles, and turtles",
-    },
-    {
-      word: "insect",
-      hint: "A small arthropod animal that has six legs and generally one or two pairs of wings",
-    },
-    {
-      word: "bird",
-      hint: "A warm-blooded egg-laying vertebrate distinguished by the possession of feathers, wings, and a beak",
-    },
-    {
-      word: "puzzle",
-      hint: "A game, toy, or problem designed to test ingenuity or knowledge",
-    },
-    {
-      word: "guitar",
-      hint: "A stringed musical instrument played with fingers or a plectrum",
-    },
-    {
-      word: "bicycle",
-      hint: "A vehicle with two wheels in tandem, propelled by pedals",
-    },
-    { word: "painting", hint: "A picture made with paint" },
-    {
-      word: "novel",
-      hint: "A long written story usually about imaginary characters and events",
-    },
-    {
-      word: "poetry",
-      hint: "Literary work in which the expression of feelings and ideas is given intensity by the use of distinctive style and rhythm",
-    },
-    {
-      word: "science",
-      hint: "The intellectual and practical activity encompassing the systematic study of the structure and behavior of the physical and natural world",
-    },
-    {
-      word: "robot",
-      hint: "A machine capable of carrying out a complex series of actions automatically",
-    },
-    {
-      word: "piano",
-      hint: "A large keyboard musical instrument with a wooden case enclosing a soundboard and metal strings",
-    },
-    {
-      word: "rocket",
-      hint: "A cylindrical projectile that can be propelled to a great height or distance by the combustion of its contents",
-    },
-    {
-      word: "jewelry",
-      hint: "Personal ornaments, such as necklaces, rings, or bracelets, made from precious metals and stones",
-    },
-    {
-      word: "camera",
-      hint: "A device for recording visual images in the form of photographs, film, or video",
-    },
-    {
-      word: "balloon",
-      hint: "A flexible bag that can be inflated with a gas, such as helium, hydrogen, nitrous oxide, oxygen, or air",
-    },
-    {
-      word: "festival",
-      hint: "A day or period of celebration, typically for religious reasons",
-    },
-    {
-      word: "mirror",
-      hint: "A reflective surface, now typically of glass coated with a metal amalgam, that reflects a clear image",
-    },
-    {
-      word: "penguin",
-      hint: "A large flightless seabird of the southern hemisphere, with black upper parts and white underparts",
-    },
-    {
-      word: "volleyball",
-      hint: "A game in which two teams hit an inflated ball over a high net using their hands",
-    },
-    {
-      word: "kangaroo",
-      hint: "A large plant-eating marsupial with powerful hind legs for leaping, native to Australia",
-    },
-    {
-      word: "library",
-      hint: "A building or room containing collections of books, periodicals, and sometimes films and recorded music",
-    },
-    {
-      word: "rainbow",
-      hint: "An arch of colors formed in the sky in certain circumstances, caused by the refraction and dispersion of the sun's light",
-    },
-  ];
+  {
+    "word": "George Washington",
+    "hint": "Led the American Revolutionary War and became the first President of the United States."
+  },
+  {
+    "word": "Thomas Jefferson",
+    "hint": "Authored the Declaration of Independence and facilitated the Louisiana Purchase."
+  },
+  {
+    "word": "Abraham Lincoln",
+    "hint": "Preserved the Union during the Civil War and emancipated the slaves with the Emancipation Proclamation."
+  },
+  {
+    "word": "Franklin D. Roosevelt",
+    "hint": "Led the United States through the Great Depression and World War II, introducing the New Deal."
+  },
+  {
+    "word": "John F. Kennedy",
+    "hint": "Famous for the Space Race and establishing the Peace Corps."
+  },
+  {
+    "word": "Lyndon B. Johnson",
+    "hint": "Significant for his 'Great Society' social service programs and the Civil Rights Act."
+  },
+  {
+    "word": "Ronald Reagan",
+    "hint": "Known for Reaganomics and ending the Cold War."
+  },
+  {
+    "word": "Barack Obama",
+    "hint": "First African American president, known for the Affordable Care Act."
+  },
+  {
+    "word": "Jimmy Carter",
+    "hint": "Noted for his role in the Camp David Accords and post-presidency humanitarian work."
+  },
+  {
+    "word": "Bill Clinton",
+    "hint": "Presided over the longest period of peacetime economic expansion."
+  },
+  {
+    "word": "Richard Nixon",
+    "hint": "Opened diplomatic relations with China but resigned due to the Watergate scandal."
+  },
+  {
+    "word": "Dwight D. Eisenhower",
+    "hint": "Led the Allied Forces in World War II and initiated the Interstate Highway System."
+  },
+  {
+    "word": "John Adams",
+    "hint": "A key figure in the American Revolution and the second President of the United States."
+  },
+  {
+    "word": "Woodrow Wilson",
+    "hint": "Created the League of Nations and won the Nobel Peace Prize."
+  },
+  {
+    "word": "Theodore Roosevelt",
+    "hint": "Known for his 'Big Stick' diplomacy and the Panama Canal."
+  },
+  {
+    "word": "Harry S. Truman",
+    "hint": "Ended World War II by ordering the use of atomic bombs and initiated the Marshall Plan."
+  },
+  {
+    "word": "George W. Bush",
+    "hint": "Led the country during the 9/11 attacks and initiated the Iraq War."
+  },
+  {
+    "word": "Andrew Jackson",
+    "hint": "Known for his role in the Indian Removal Act and founder of the Democratic Party."
+  },
+  {
+    "word": "Thomas Jefferson",
+    "hint": "Commissioned the Lewis and Clark Expedition."
+  },
+  {
+    "word": "Franklin D. Roosevelt",
+    "hint": "Established the Social Security Act."
+  },
+  {
+    "word": "Theodore Roosevelt",
+    "hint": "Known for his conservation efforts and the establishment of National Parks."
+  },
+  {
+    "word": "Herbert Hoover",
+    "hint": "Led major relief efforts in Europe during and after World War I."
+  },
+  {
+    "word": "James K. Polk",
+    "hint": "Significant for the expansion of the U.S. territory through the Mexican-American War."
+  },
+  {
+    "word": "Thomas Jefferson",
+    "hint": "Founder of the University of Virginia."
+  },
+  {
+    "word": "William McKinley",
+    "hint": "Led the nation to victory in the Spanish-American War and supported the Gold Standard Act."
+  },
+  {
+    "word": "John F. Kennedy",
+    "hint": "Initiated the Apollo program aiming to land a man on the moon."
+  },
+  {
+    "word": "Gerald Ford",
+    "hint": "Known for his controversial pardon of Richard Nixon."
+  },
+  {
+    "word": "Calvin Coolidge",
+    "hint": "Known for his laissez-faire approach to the booming economy of the 1920s."
+  },
+  {
+    "word": "James Monroe",
+    "hint": "Famous for the Monroe Doctrine, opposing European colonialism in the Americas."
+  },
+  {
+    "word": "Ulysses S. Grant",
+    "hint": "Led the Union Army to victory in the Civil War and supported Reconstruction."
+  },
+  {
+    "word": "James Madison",
+    "hint": "Father of the Constitution and the Bill of Rights."
+  },
+];
 
 let currentWordIndex = 0;
 let score = 0;
@@ -229,7 +173,7 @@ function generateWordOptions(correctWord, wordList) {
   let options = new Set([correctWord]);
   let tempWordList = [...wordList]; // Create a copy of wordList
 
-  while (options.size < 10 && tempWordList.length > 0) {
+  while (options.size < 5 && tempWordList.length > 0) {
     let randomIndex = Math.floor(Math.random() * tempWordList.length);
     let randomWord = tempWordList[randomIndex].word;
 
